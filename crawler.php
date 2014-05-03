dba3d3a352a7c8150e18ca1df456be077c831c59
David Ecker <dcecker@gmail.com>
2014-05-03 00:49:26 -0400

    this one should work

--- crawler.php
+++ crawler.php
@@ -1,6 +1,6 @@
 <?php
 require  'facebook-php-sdk/src/facebook.php';
-$facebook = new Facebook(array('appId' => 'YOUR_APP_ID', 'secret' => 'YOUR_SECRET'));	     
+$facebook = new Facebook(array('appId' => '521884561263250', 'secret' => 'cc95d1118d819cc351c4d8319efe301b'));
 //https://graph.facebook.com/oauth/access_token?client_id=YOUR_APP_ID&client_secret=YOUR_APP_SECRET&grant_type=client_credentials
 
 //test for curl
@@ -13,12 +13,11 @@ else{
 
 try{
 	//get the web app database
-	$fb_db = new PDO('mysql:host=localhost;dbname=dbname', 'usrname', 'pw');
+	$fb_db = new PDO('mysql:host=localhost;dbname=here_doggie', 'root', 'SeniorDesign14');
 	$fb_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$urls = $fb_db->query('SELECT * FROM facebook_urls');
-	$searches = $fb_db->query('SELECT date_lost FROM searches');
+	//$urls = $fb_db->query('SELECT * FROM facebook_urls WHERE ');
 	$search_ids = $fb_db->query('SELECT id FROM searches');
-	
+	/*
 	foreach($searches  as  $search){
 		//echo "<pre>";
 		//var_dump($search);
@@ -27,15 +26,26 @@ try{
 		//next 2 lines remove timestamp from date string:
 		$date_lost = explode(" ", $date_lost);
 		$date_lost = $date_lost[0];
-		
 	}
+	*/
 	
-	//this finds the most recent search ID and saves it as a variable used later inside the images folder
+	//not sure if this is needed,
+	//gets last search id and saves it in a variable, bad way of doing this
 	foreach($search_ids as $s_id){
 		$search_id = $s_id['id'];
-		//echo $search_id . PHP_EOL;
 	}
-	
+
+	//remove me!!
+	$search_id = 22;
+	//get date lost
+	$searches = $fb_db->query('SELECT date_lost FROM searches WHERE id='.$search_id);
+	foreach($searches as $search){
+		$date_lost = $search['date_lost'];
+		//remove timestamp
+		$date_lost = explode(" ", $date_lost);
+		$date_lost = $date_lost[0];
+	}
+
 	$dog_array = array();
 	
 	$path =  "images/".$search_id."/";
@@ -44,18 +54,28 @@ try{
     		mkdir($path, 0775, true);
 	}
 	
+	//get the urls
+	$urls = $fb_db->query('SELECT * FROM facebook_urls WHERE search_id='.$search_id);
+
 	foreach($urls  as  $row) {
 		//echo "<pre>";
 		//var_dump($row);
 		//echo "</pre>";
 		//get facebook url from database and remove "http://www.facebook.com/" from it
 		$fb_url = $row['url'];
+		$fb_url = strtolower($fb_url);
+		if (strpos($fb_url, 'https://') === false){
+			$fb_url = "https://".$fb_url;
+		}
+		echo "Getting ready to crawl sanitized URL: ".$fb_url.PHP_EOL;
 		$fb_url = str_replace('https://www.facebook.com/', '', $fb_url);
+		echo"Trimmed url: ".$fb_url.PHP_EOL;
 		//create an array for this url
 		$temp_array = array();
 		
-		//$request = $fb_url.'/photos/uploaded?fields=created_time,link,images&limit=1';
-		$request = $fb_url.'/photos/uploaded?fields=source,created_time,link&since='.$date_lost;
+		//$request = $fb_url.'/photos/uploaded?fields=created_time,link,images,name&limit=1';
+		var_dump($date_lost);
+		$request = $fb_url.'/photos/uploaded?fields=source,created_time,link,name&since='.$date_lost;
 		//echo  $request;
 		$response = $facebook->api($request, 'GET');
 		//echo "<pre>";
@@ -81,15 +101,22 @@ try{
 					//add image info to array
 					$temp_temp_array['path'] = $dirname."/";
 					$temp_temp_array['filename'] = $counter.'.jpg';
+					echo "Image downloaded and added to array: ".$counter.PHP_EOL;
 					$counter++;
 				}
-				if($key == 'created_time'){
+				if($key == "created_time"){
 					//add the created_time (day posted) to the array
 					$temp_temp_array['dateAdded'] = $value;
+					echo "Date_Added added to array.".PHP_EOL;
 				}
-				if($key == 'link'){
+				if($key == "link"){
 					//add the link to the dog array
 					$temp_temp_array['listingURL'] = $value;
+					echo "Listing URL added to array.".PHP_EOL;
+				}
+				if($key == "name"){
+					$temp_temp_array['description'] = $value;
+					echo "Description added to array.".PHP_EOL;
 				}
 				//echo  "</ul>";
 					
@@ -100,30 +127,59 @@ try{
 		$dog_array[$fb_url] = $temp_array;
 	}
 	
+	/* For putting photos in dog db
+	$dbname = 'dbname';
+	$user = 'test_db';
+	$pass = 'password';
+	$dog_db = new PDO('mysql:host=localhost;dbname='.$dbname , $user, $pass);
+	$dog_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql_dogs = "INSERT INTO dogs (listingURL, dateAdded) values (:listingURL, :dateAdded)";
+	$sql_photos = "INSERT INTO photos (filename, path, dogID) values (:filename, :path, :dogID)";
+	
+	foreach($dog_array as $fb_site){
+		foreach($fb_site as $dog){
+			//prepare sql insert statements
+			$query = $dog_db->prepare($sql_dogs);
+			$query->bindParam(':listingURL', $dog['listingURL']);
+			$query->bindParam(':dateAdded', $dog['dateAdded']);
+			$query->execute();
+			
+			$dog_id = $dog_db->lastInsertId();
+			
+			$query = $dog_db->prepare($sql_photos);
+			$query->bindParam(':filename', $dog['filename']);
+			$query->bindParam(':path', $dog['path']);
+			$query->bindParam(':dogID', $dog_id);
+			$query->execute();
+		}
+	}
+	*/
+	
 	// For putting photos in cache db
 	//Create cache db
-	$cache_db = new PDO('sqlite:cache.sqlite3');
+	$cache_db = new PDO('sqlite:cache'.$search_id.'.sqlite3');
 	
 	//set errormode
 	$cache_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 	//create tables
 	$cache_db->exec("CREATE TABLE IF NOT EXISTS dogs(
-		dogID		int primary key not null,
+		id		integer primary key autoincrement,
 		listingURL	varchar(255) not null,
-		dateAdded	date not null
+		dateAdded	date not null,
+		description	text
 		)");
 		
 	$cache_db->exec("CREATE TABLE IF NOT EXISTS photos(
-		photoID		int primary key not null,
+		photoID		integer primary key autoincrement,
 		filename	varchar(255) not null,
 		path		varchar(255) not null,
-		dogID		int not null,
-		foreign key (dogID) references dogs(dogID)
+		dogID		integer unsigned not null,
+		foreign key (dogID) references dogs(id)
 		)");
 		
 	//insert data into cache db
-	$sql_dogs = "INSERT INTO dogs (listingURL, dateAdded) values (:listingURL, :dateAdded)";
+	$sql_dogs = "INSERT INTO dogs (listingURL, dateAdded, description) values (:listingURL, :dateAdded, :description)";
 	$sql_photos = "INSERT INTO photos (filename, path, dogID) values (:filename, :path, :dogID)";
 	
 	foreach($dog_array as $fb_site){
@@ -132,10 +188,11 @@ try{
 			$query = $cache_db->prepare($sql_dogs);
 			$query->bindParam(':listingURL', $dog['listingURL']);
 			$query->bindParam(':dateAdded', $dog['dateAdded']);
+			$query->bindParam(':description', $dog['description']);
 			$query->execute();
 			
 			$dog_id = $cache_db->lastInsertId();
-			
+			echo('DOG ID: '.$dog_id.PHP_EOL);
 			$query = $cache_db->prepare($sql_photos);
 			$query->bindParam(':filename', $dog['filename']);
 			$query->bindParam(':path', $dog['path']);
@@ -157,6 +214,7 @@ try{
 }
 catch(PDOException $e){
 	echo  $e->getMessage() . PHP_EOL; 
+	echo 'On line: ' . $e->getLine() . PHP_EOL;
 }
 
 function  grab_image($url,$saveto){
